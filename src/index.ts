7ea368b07ad43d205f3bfbcccc1e88a480d8eef3
Yash Gandhi <gandhiyash1381997@gmail.com>
2019-11-05 15:49:41 +0530

    basic jest setup

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {Users} from "./entity/User";
+import { createConnection } from "typeorm";
+import { GraphQLServer } from "graphql-yoga";
+import { importSchema } from "graphql-import";
+import * as path from "path";
+import resolvers from "./resolvers/resolvers";
 
-createConnection().then(async connection => {
+const connectionObject =
+  process.env.NODE_ENV === "4000"
+    ? require("../ormconfig.json")
+    : require("../ormconfigtest.json");
 
-    console.log("Inserting a new user into the database...");
-   /* const user = new Users();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+export async function startServer() {
+  await createConnection(connectionObject);
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(Users);
-    console.log("Loaded users: ", users);*/
+  const typeDefs = importSchema(path.join(__dirname, "schema/schema.graphql"));
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+  const server = new GraphQLServer({ typeDefs, resolvers });
+  console.log(process.env.NODE_ENV, 14);
+  await server.start({
+    port: process.env.NODE_ENV
+  });
+  console.log("Server is running...");
+}
 
-}).catch(error => console.log(error));
+startServer();
---
+++ src/resolvers/resolvers.ts
@@ -0,0 +1,29 @@
+import "../types/schema";
+import { ResolverMap } from "../types/graphqlTypesUtils";
+import * as bcrypt from "bcrypt";
+import { Users } from "../entity/Users";
+
+const resolvers: ResolverMap = {
+  Query: {
+    hello: (_: any, { name }: GQL.IHelloOnQueryArguments) => `hello ${name}`
+  },
+  Mutation: {
+    register: async (
+      _: any,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const passwordSalt = parseInt(process.env.PASSWORD_SALT || "0", 10) || 2;
+      console.log(passwordSalt);
+      const hashedPassword = await bcrypt.hash(password, passwordSalt);
+      const user = Users.create({
+        email,
+        password: hashedPassword
+      });
+
+      await user.save();
+      return true;
+    }
+  }
+};
+
+export default resolvers;
